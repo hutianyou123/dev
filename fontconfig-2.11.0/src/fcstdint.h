e68f160c26909b03d07e04c831cf3b1e0c7d0f48
TianyouHu <tianyouhu@pptv.com>
2017-08-04 19:22:12 +0800

    dev

---
+++ fontconfig-2.11.0/src/fcstdint.h
@@ -0,0 +1,9 @@
+#ifndef _FONTCONFIG_SRC_FCSTDINT_H
+#define _FONTCONFIG_SRC_FCSTDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "fontconfig 2.11.0"
+/* generated using gnu compiler gcc (GCC) 4.4.6 20110731 (Red Hat 4.4.6-3) */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
